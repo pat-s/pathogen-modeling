213a5375960c114430ff2d4d52cb805e4223a89d
pat-s <patrick.schratz@gmail.com>
2019-03-14 16:42:08 +0100

    use only 20 cpus, use standard seed

--- R/benchmark-functions.R
+++ R/benchmark-functions.R
@@ -3,19 +3,19 @@ benchmark_custom <- function(task, learner, resampling) {
   if (grepl("kknn", learner$id)) {
     cores = ignore(16)
   } else {
-    cores = ignore(32)
+    cores = ignore(20)
   }
 
   parallelStart(
     mode = "multicore", cpus = cores, level = "mlr.resample",
-    mc.set.seed = TRUE
+    mc.set.seed = FALSE
   )
-  set.seed(12345, kind = "L'Ecuyer-CMRG")
+  set.seed(12345)
 
   bmr <- benchmark(learner, task,
-                   models = ignore(T),
+                   models = ignore(FALSE),
                    keep.pred = ignore(TRUE),
-                   keep.extract = ignore(FALSE),
+                   keep.extract = ignore(TRUE),
                    resampling = resampling,
                    show.info = ignore(TRUE), measures = list(brier, timetrain)
   )
