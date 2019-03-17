7dede59b2dae37236d6164ca9f891d84589db587
pat-s <patrick.schratz@gmail.com>
2019-03-17 18:56:16 +0100

    group benchmark results

--- analysis/benchmark/aggregate-results.R
+++ analysis/benchmark/aggregate-results.R
@@ -1,20 +1,61 @@
-bmr_aggregated = drake_plan(bm_sp_sp_gam = mergeBenchmarkResults(list(bm_sp_sp_armillaria_gam, bm_sp_sp_heterobasidion_gam,
-                                                                      bm_sp_sp_diplodia_gam, bm_sp_sp_fusarium_gam)),
-                            bm_sp_nsp_gam = mergeBenchmarkResults(list(bm_sp_nsp_armillaria_gam, bm_sp_nsp_heterobasidion_gam,
-                                                                       bm_sp_nsp_diplodia_gam, bm_sp_nsp_fusarium_gam)),
-                            bm_sp_non_gam = mergeBenchmarkResults(list(bm_sp_non_armillaria_gam, bm_sp_non_heterobasidion_gam,
-                                                                       bm_sp_non_diplodia_gam, bm_sp_non_fusarium_gam)),
-                            bm_nsp_nsp_gam = mergeBenchmarkResults(list(bm_nsp_nsp_armillaria_gam, bm_nsp_nsp_heterobasidion_gam,
-                                                                        bm_nsp_nsp_diplodia_gam, bm_nsp_nsp_fusarium_gam)),
-                            bm_nsp_non_gam = mergeBenchmarkResults(list(bm_nsp_non_armillaria_gam, bm_nsp_non_heterobasidion_gam,
-                                                                        bm_nsp_non_diplodia_gam, bm_nsp_non_fusarium_gam)),
-                            bm_sp_sp_all = mergeBenchmarkResults(list(bm_sp_sp_armillaria_rf, bm_sp_sp_diplodia_rf, bm_sp_sp_fusarium_rf, bm_sp_sp_heterobasidion_rf,
-                                                                      bm_sp_sp_armillaria_svm, bm_sp_sp_diplodia_svm, bm_sp_sp_fusarium_svm, bm_sp_sp_heterobasidion_svm,
-                                                                      bm_sp_sp_armillaria_brt, bm_sp_sp_diplodia_brt, bm_sp_sp_fusarium_brt, bm_sp_sp_heterobasidion_brt,
-                                                                      bm_sp_sp_armillaria_kknn, bm_sp_sp_diplodia_kknn, bm_sp_sp_fusarium_kknn, bm_sp_sp_heterobasidion_kknn,
-                                                                      bm_sp_sp_armillaria_xgboost, bm_sp_sp_diplodia_xgboost, bm_sp_sp_fusarium_xgboost, bm_sp_sp_heterobasidion_xgboost,
-                                                                      bm_sp_sp_armillaria_gam, bm_sp_sp_diplodia_gam, bm_sp_sp_fusarium_gam, bm_sp_sp_heterobasidion_gamn))
-                            # bm_sp_sp_all_reduced = stripname(bm_sp_sp_all, "extract") %>%
-                            #   set_class("BenchmarkResult")
+bmr_aggregated = drake_plan(bm_sp_sp_diplodia = mergeBenchmarkResults(list(bm_sp_sp_diplodia_gam,
+                                                                           bm_sp_sp_diplodia_kknn, bm_sp_sp_diplodia_rf,
+                                                                           bm_sp_sp_diplodia_svm, bm_sp_sp_diplodia_brt)),
+
+                            bm_sp_nsp_diplodia = mergeBenchmarkResults(list(bm_sp_nsp_diplodia_gam,
+                                                                            bm_sp_nsp_diplodia_kknn, bm_sp_nsp_diplodia_rf,
+                                                                            bm_sp_nsp_diplodia_svm, bm_sp_nsp_diplodia_brt)),
+
+                            bm_nsp_nsp_diplodia = mergeBenchmarkResults(list(bm_nsp_nsp_diplodia_gam,
+                                                                             bm_nsp_nsp_diplodia_kknn, bm_nsp_nsp_diplodia_rf,
+                                                                             bm_nsp_nsp_diplodia_svm, bm_nsp_nsp_diplodia_brt)),
+
+                            bm_sp_non_diplodia = mergeBenchmarkResults(list(bm_sp_non_diplodia_gam, bm_sp_non_diplodia_glm,
+                                                                            bm_sp_non_diplodia_kknn, bm_sp_non_diplodia_rf,
+                                                                            bm_sp_non_diplodia_svm, bm_sp_non_diplodia_brt)),
+
+                            bm_nsp_non_diplodia = mergeBenchmarkResults(list(bm_nsp_non_diplodia_gam, bm_nsp_non_diplodia_glm,
+                                                                             bm_nsp_non_diplodia_kknn, bm_nsp_non_diplodia_rf,
+                                                                             bm_nsp_non_diplodia_svm, bm_nsp_non_diplodia_brt))
+
+
 )
 
+no_extract = drake_plan(
+  no_extract = target(
+    remove_extract(bm_result),
+    transform = cross(bm_result = c(bm_sp_sp_diplodia, bm_sp_nsp_diplodia,
+                                    bm_nsp_nsp_diplodia, bm_sp_non_diplodia,
+                                    bm_nsp_non_diplodia,
+
+                                    bm_sp_sp_armillaria, bm_sp_nsp_armillaria,
+                                    bm_nsp_nsp_armillaria, bm_sp_non_armillaria,
+                                    bm_nsp_non_armillaria,
+
+                                    bm_sp_sp_fusarium, bm_sp_nsp_fusarium,
+                                    bm_nsp_nsp_fusarium, bm_sp_non_fusarium,
+                                    bm_nsp_non_fusarium,
+
+                                    bm_sp_sp_heterobasidion, bm_sp_nsp_heterobasidion,
+                                    bm_nsp_nsp_heterobasidion, bm_sp_non_heterobasidion,
+                                    bm_nsp_non_heterobasidion))
+  )
+)
+
+bm_all_pathogens = drake_plan(
+  bm_all_pathogens = mergeBenchmarkResults(list(bm_sp_sp_diplodia, bm_sp_nsp_diplodia,
+                                                bm_nsp_nsp_diplodia, bm_sp_non_diplodia,
+                                                bm_nsp_non_diplodia,
+
+                                                bm_sp_sp_armillaria, bm_sp_nsp_armillaria,
+                                                bm_nsp_nsp_armillaria, bm_sp_non_armillaria,
+                                                bm_nsp_non_armillaria,
+
+                                                bm_sp_sp_fusarium, bm_sp_nsp_fusarium,
+                                                bm_nsp_nsp_fusarium, bm_sp_non_fusarium,
+                                                bm_nsp_non_fusarium,
+
+                                                bm_sp_sp_heterobasidion, bm_sp_nsp_heterobasidion,
+                                                bm_nsp_nsp_heterobasidion, bm_sp_non_heterobasidion,
+                                                bm_nsp_non_heterobasidion))
+)
