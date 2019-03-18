d233867162d51be38e229402823058ff939cbd83
pat-s <patrick.schratz@gmail.com>
2019-03-18 18:29:16 +0100

    create partitioning figure

--- analysis/visualization/vis-partitions.R
+++ analysis/visualization/vis-partitions.R
@@ -1,7 +1,6 @@
 plot_partitions = vis_partitions(diplodia_data, bm_sp_non_diplodia_glm, bm_nsp_non_diplodia_glm)
 
-img_plot_partitions = save_plot(file_out("analysis/figures/spcv_nspcv_folds.png"),
-                                 plot_partitions, ncol = 3, nrow = 4)
-
+img_plot_partitions = save_plot_custom(plot_partitions, "analysis/figures/spcv_nspcv_folds.png",
+                                       ncol = 3, nrow = 4)
 
 img_plot_opt_paths = save_plot_opt_paths()
