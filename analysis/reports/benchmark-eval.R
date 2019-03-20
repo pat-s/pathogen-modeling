6d0ca5599a894fef7ac5dfd03c43004bfb17a2d2
pat-s <patrick.schratz@gmail.com>
2019-03-20 11:09:52 +0100

    add more reports

--- analysis/reports/benchmark-eval.R
+++ analysis/reports/benchmark-eval.R
@@ -1,4 +1,4 @@
-reports = drake_plan(
+reports_plan = drake_plan(
   # Work around https://github.com/rstudio/rmarkdown/issues/1268
   benchmark_evaluation_report_diplodia = knitr::knit(
     #opts_knit$set(base.dir = "analysis/rmd/"),
@@ -8,7 +8,19 @@ reports = drake_plan(
   benchmark_evaluation_report_all = knitr::knit(
     knitr_in("analysis/rmd/benchmark-eval-all.Rmd"),
     output = file_out("analysis/rmd/benchmark-eval-all.md"), # Important: must be the output file
-    fig.path = ('analysis/rmd/figure'),
     quiet = TRUE),
+  visualize_partitions = knitr::knit(
+    knitr_in("analysis/rmd/visualize-partitions.Rmd"),
+    output = file_out("analysis/rmd/visualize-partitions.md"), # Important: must be the output file
+    quiet = TRUE),
+  visualize_opt_paths = knitr::knit(
+    knitr_in("analysis/rmd/vis-opt-paths.Rmd"),
+    output = file_out("analysis/rmd/vis-opt-paths.md"), # Important: must be the output file
+    quiet = TRUE),
+  visualize_tuning_effects = knitr::knit(
+    knitr_in("analysis/rmd/vis-tuning-effects.Rmd"),
+    output = file_out("analysis/rmd/vis-tuning-effects.md"), # Important: must be the output file
+    quiet = TRUE),
+
   strings_in_dots = "literals"
 )
