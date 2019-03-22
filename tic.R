496e7005f23d146f94f087494d3d7cc4de1277d3
pat-s <patrick.schratz@gmail.com>
2019-03-22 13:11:55 +0100

    travis: only do rcmdcheck

--- tic.R
+++ tic.R
@@ -1,1 +1,2 @@
-add_package_checks(error_on = "error", args = c("--no-tests", "--no-manual"))
+get_stage("script") %>%
+  add_step(step_rcmdcheck(error_on = "error", args = c("--no-tests", "--no-manual")))
