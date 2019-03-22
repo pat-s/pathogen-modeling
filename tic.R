e25078cec669ca67e30751a939a709b573b7d52a
pat-s <patrick.schratz@gmail.com>
2019-03-22 14:51:04 +0100

    trim travis

--- tic.R
+++ tic.R
@@ -1,2 +1,3 @@
 get_stage("script") %>%
-  add_step(step_rcmdcheck(error_on = "error", args = c("--no-tests", "--no-manual")))
+  add_step(step_rcmdcheck(error_on = "error", args = c("--no-tests", "--no-manual"),
+                          prepare_call = NULL))
