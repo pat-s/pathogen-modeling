78624aa64132530a893c9d1caae8d31bd16fec4e
pat-s <patrick.schratz@gmail.com>
2019-04-12 12:03:14 +0200

    simplify package checking

--- tic.R
+++ tic.R
@@ -1,5 +1,1 @@
-get_stage("script") %>%
-  add_step(step_rcmdcheck(error_on = "error", args = c("--no-tests", "--no-manual"),
-                          prepare_call = function() {
-                            verify_install("rcmdcheck")
-                          }))
+do_package_checks(error_on = "error", args = c("--no-tests", "--no-manual")
