15a8c158223fc7f1553e2d1b5838660b33079396
pat-s <patrick.schratz@gmail.com>
2019-05-06 18:03:58 +0200

    rename

--- analysis/reports/reports.R
+++ analysis/reports/reports.R
@@ -1,26 +1,33 @@
 reports_plan = drake_plan(
-  # Work around https://github.com/rstudio/rmarkdown/issues/1268
+
+  # Workaround https://github.com/rstudio/rmarkdown/issues/1268
+
   benchmark_evaluation_report_diplodia = knitr::knit(
     #opts_knit$set(base.dir = "analysis/rmd/"),
     knitr_in("analysis/rmd/benchmark-eval-diplodia.Rmd"),
     output = file_out("analysis/rmd/benchmark-eval-diplodia.md"), # Important: must be the output file
     quiet = TRUE),
+
   benchmark_evaluation_report_all = knitr::knit(
     knitr_in("analysis/rmd/benchmark-eval-all.Rmd"),
     output = file_out("analysis/rmd/benchmark-eval-all.md"), # Important: must be the output file
     quiet = TRUE),
+
   visualize_partitions = knitr::knit(
     knitr_in("analysis/rmd/visualize-partitions.Rmd"),
     output = file_out("analysis/rmd/visualize-partitions.md"), # Important: must be the output file
     quiet = TRUE),
+
   visualize_opt_paths = knitr::knit(
     knitr_in("analysis/rmd/vis-opt-paths.Rmd"),
     output = file_out("analysis/rmd/vis-opt-paths.md"), # Important: must be the output file
     quiet = TRUE),
+
   visualize_tuning_effects = knitr::knit(
     knitr_in("analysis/rmd/vis-tuning-effects.Rmd"),
     output = file_out("analysis/rmd/vis-tuning-effects.md"), # Important: must be the output file
     quiet = TRUE),
+
   prediction_maps = knitr::knit(
     knitr_in("analysis/rmd/prediction-maps.Rmd"),
     output = file_out("analysis/rmd/prediction-maps.md"), # Important: must be the output file
