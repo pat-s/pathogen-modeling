a2c62bdaab00c21f1d0fb7139d1091b34b4764f6
pat-s <patrick.schratz@gmail.com>
2019-05-14 17:20:23 +0200

    upd reports.R

--- analysis/reports/reports.R
+++ analysis/reports/reports.R
@@ -1,36 +1,33 @@
 reports_plan = drake_plan(
 
-  # Workaround https://github.com/rstudio/rmarkdown/issues/1268
-
-  benchmark_evaluation_report_diplodia = knitr::knit(
-    #opts_knit$set(base.dir = "analysis/rmd/"),
-    knitr_in("analysis/rmd/benchmark-eval-diplodia.Rmd"),
-    output = file_out("analysis/rmd/benchmark-eval-diplodia.md"), # Important: must be the output file
+  benchmark_diplodia = knitr::knit(
+    knitr_in("analysis/rmd/paper/benchmark-diplodia.Rmd"),
+    output = file_out("analysis/rmd/paper/benchmark-diplodia.md"),
     quiet = TRUE),
 
-  benchmark_evaluation_report_all = knitr::knit(
-    knitr_in("analysis/rmd/benchmark-eval-all.Rmd"),
-    output = file_out("analysis/rmd/benchmark-eval-all.md"), # Important: must be the output file
+  benchmark_all_pathogens = knitr::knit(
+    knitr_in("analysis/rmd/project/benchmark-all-pathogens.Rmd"),
+    output = file_out("analysis/rmd/project/benchmark-all-pathogens.md"),
     quiet = TRUE),
 
   visualize_partitions = knitr::knit(
-    knitr_in("analysis/rmd/visualize-partitions.Rmd"),
-    output = file_out("analysis/rmd/visualize-partitions.md"), # Important: must be the output file
+    knitr_in("analysis/rmd/paper/visualize-partitions.Rmd"),
+    output = file_out("analysis/rmd/paper/visualize-partitions.md"),
     quiet = TRUE),
 
   visualize_opt_paths = knitr::knit(
-    knitr_in("analysis/rmd/vis-opt-paths.Rmd"),
-    output = file_out("analysis/rmd/vis-opt-paths.md"), # Important: must be the output file
+    knitr_in("analysis/rmd/paper/vis-opt-paths.Rmd"),
+    output = file_out("analysis/rmd/paper/vis-opt-paths.md"),
     quiet = TRUE),
 
   visualize_tuning_effects = knitr::knit(
-    knitr_in("analysis/rmd/vis-tuning-effects.Rmd"),
-    output = file_out("analysis/rmd/vis-tuning-effects.md"), # Important: must be the output file
+    knitr_in("analysis/rmd/paper/vis-tuning-effects.Rmd"),
+    output = file_out("analysis/rmd/paper/vis-tuning-effects.md"),
     quiet = TRUE),
 
   prediction_maps = knitr::knit(
-    knitr_in("analysis/rmd/prediction-maps.Rmd"),
-    output = file_out("analysis/rmd/prediction-maps.md"), # Important: must be the output file
+    knitr_in("analysis/rmd/project/pathogen-prediction-report.Rmd"),
+    output = file_out("analysis/rmd/project/pathogen-prediction-report.md"),
     quiet = TRUE),
 
   strings_in_dots = "literals"
