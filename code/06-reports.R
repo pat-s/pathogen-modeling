6af418121918e9715dca25de66afbe897e7453aa
pat-s <patrick.schratz@gmail.com>
2019-05-23 10:46:05 +0200

    update reports

--- code/06-reports.R
+++ code/06-reports.R
@@ -1,39 +1,18 @@
 reports_plan = drake_plan(
 
-  pathogens_performance_diplodia = knitr::knit(
-    knitr_in("analysis/rmd/paper/pathogens-performance-diplodia.Rmd"),
-    output = file_out("analysis/rmd/paper/pathogens-performance-diplodia.md"),
-    quiet = TRUE),
+  benchmark_diplodia = wflow_build(knitr_in("analysis/benchmark_diplodia.Rmd"), view = FALSE),
 
-  pathogens_performance = knitr::knit(
-    knitr_in("analysis/rmd/project/pathogens-performance.Rmd"),
-    output = file_out("analysis/rmd/project/pathogens-performance.md"),
-    quiet = TRUE),
+  benchmark_pathogens = wflow_build(knitr_in("analysis/benchmark_pathogens.Rmd"), view = FALSE),
 
-  pathogens_prediction = knitr::knit(
-    knitr_in("analysis/rmd/project/pathogen-prediction.Rmd"),
-    output = file_out("analysis/rmd/project/pathogens-prediction.md"),
-    quiet = TRUE),
+  prediction_pathogens = wflow_build(knitr_in("analysis/prediction-pathogens.Rmd"), view = FALSE),
 
-  pathogens_prediction_debugging = knitr::knit(
-    knitr_in("analysis/rmd/project/pathogens-prediction-debugging.Rmd"),
-    output = file_out("analysis/rmd/project/pathogens-prediction-debugging.md"),
-    quiet = TRUE),
+  pathogen_maps_debugging = wflow_build(knitr_in("analysis/pathogen-maps-debugging.Rmd"), view = FALSE),
 
-  visualize_partitions = knitr::knit(
-    knitr_in("analysis/rmd/paper/visualize-partitions.Rmd"),
-    output = file_out("analysis/rmd/paper/visualize-partitions.md"),
-    quiet = TRUE),
+  vis_partitions = wflow_build(knitr_in("analysis/vis-partitions.Rmd"), view = FALSE),
 
-  visualize_opt_paths = knitr::knit(
-    knitr_in("analysis/rmd/paper/vis-opt-paths.Rmd"),
-    output = file_out("analysis/rmd/paper/vis-opt-paths.md"),
-    quiet = TRUE),
+  vis_opt_paths = wflow_build(knitr_in("analysis/vis-opt-paths.Rmd"), view = FALSE),
 
-  visualize_tuning_effects = knitr::knit(
-    knitr_in("analysis/rmd/paper/vis-tuning-effects.Rmd"),
-    output = file_out("analysis/rmd/paper/vis-tuning-effects.md"),
-    quiet = TRUE),
+  vis_tuning_effects = wflow_build(knitr_in("analysis/vis-tuning-effects.Rmd"), view = FALSE),
 
   strings_in_dots = "literals"
 )
