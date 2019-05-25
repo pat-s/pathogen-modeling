dac5d6a832ccde6783dc6c5285fe3acfbbf3cf7b
pat-s <patrick.schratz@gmail.com>
2019-05-25 18:04:19 +0200

    upd reports

--- code/06-reports.R
+++ code/06-reports.R
@@ -1,18 +1,18 @@
 reports_plan = drake_plan(
 
-  benchmark_diplodia = wflow_build(knitr_in("analysis/benchmark-diplodia.Rmd"), view = FALSE),
+  benchmark_diplodia = wflow_publish(knitr_in("analysis/benchmark-diplodia.Rmd"), view = FALSE),
 
-  benchmark_pathogens = wflow_build(knitr_in("analysis/benchmark-pathogens.Rmd"), view = FALSE),
+  benchmark_pathogens = wflow_publish(knitr_in("analysis/benchmark-pathogens.Rmd"), view = FALSE),
 
-  prediction_pathogens = wflow_build(knitr_in("analysis/prediction-pathogens.Rmd"), view = FALSE),
+  prediction_pathogens = wflow_publish(knitr_in("analysis/prediction-pathogens.Rmd"), view = FALSE),
 
-  pathogen_maps_debugging = wflow_build(knitr_in("analysis/pathogen-maps-debugging.Rmd"), view = FALSE),
+  pathogen_maps_debugging = wflow_publish(knitr_in("analysis/pathogen-maps-debugging.Rmd"), view = FALSE),
 
-  vis_partitions = wflow_build(knitr_in("analysis/vis-partitions.Rmd"), view = FALSE),
+  vis_partitions = wflow_publish(knitr_in("analysis/vis-partitions.Rmd"), view = FALSE),
 
-  vis_opt_paths = wflow_build(knitr_in("analysis/vis-opt-paths.Rmd"), view = FALSE),
+  vis_opt_paths = wflow_publish(knitr_in("analysis/vis-opt-paths.Rmd"), view = FALSE),
 
-  vis_tuning_effects = wflow_build(knitr_in("analysis/vis-tuning-effects.Rmd"), view = FALSE),
+  vis_tuning_effects = wflow_publish(knitr_in("analysis/vis-tuning-effects.Rmd"), view = FALSE),
 
   strings_in_dots = "literals"
 )
